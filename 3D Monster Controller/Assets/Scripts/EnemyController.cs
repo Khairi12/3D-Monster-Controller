0d3b222e7bd0c323f37e5d4b96fb89678586802f
BuildTools <khairiissa428@gmail.com>
2018-09-14 00:56:19 -0500

    skills / run / attack fix

--- 3D Monster Controller/Assets/Scripts/EnemyController.cs
+++ 3D Monster Controller/Assets/Scripts/EnemyController.cs
@@ -1,16 +1,113 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+public enum MonsterMoveState { IDLE, CHASING, SEEKING }
 
 public class EnemyController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [HideInInspector] public bool movementEnabled = true;
+
+    public float defaultSpeed = 3f;         // default speed
+    public float chaseSpeed = 6f;           // chase speed
+
+    public float idleDuration = 1;
+    public float stopDistance = 1;
+
+    public float fovRange = 50f;            // max visible range
+    public float fovAngle = 160f;           // max visible FOV
+
+    private MonsterMoveState moveState;     // current state of movement
+    private NavMeshAgent navagent;          // monster navigation agent
+    private Transform playerTransform;      // player transform for player position
+    private Vector3 destination;            // current destination
+
+    private float outgoingStopDist;         // stop distance after calculations
+    private float outGoingIdleTime;         // amount of time left to idle
+    private float outgoingRange;            // visible range after calculations
+
+    public void SetDestination(Vector3 newDestination) {
+        destination = newDestination;
+        navagent.SetDestination(destination);
+    }
+
+    private void Awake() {
+        navagent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Start() {
+        moveState = MonsterMoveState.IDLE;
+        navagent.speed = defaultSpeed;
+        outgoingStopDist = stopDistance;
+        outGoingIdleTime = idleDuration;
+        outgoingRange = fovRange;
+    }
+
+    private bool IsPlayerVisible(Vector3 transformPos, Vector3 playerPos, float range) {
+        RaycastHit hit;
+        float curAngle = Vector3.Angle(playerTransform.position - transform.position, transform.forward);
+
+        if (Vector3.Distance(transformPos, playerPos) < range) {
+            if (Physics.Raycast(transformPos, playerPos - transformPos, out hit)) {
+                if (hit.collider.tag == "Player") {
+                    if (curAngle < fovAngle * 0.5f) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void Idle() {
+        outGoingIdleTime -= Time.deltaTime;
+
+        if (outGoingIdleTime <= 0f) {
+            navagent.isStopped = false;
+            moveState = MonsterMoveState.SEEKING;
+            outGoingIdleTime = idleDuration;
+        }
+        else if (IsPlayerVisible(transform.position, playerTransform.position, outgoingRange)) {
+            navagent.isStopped = false;
+            navagent.speed = chaseSpeed;
+            moveState = MonsterMoveState.CHASING;
+            outGoingIdleTime = idleDuration;
+        }
+    }
+
+    private void Seek() {
+        if (IsPlayerVisible(transform.position, playerTransform.position, outgoingRange)) {
+            navagent.speed = chaseSpeed;
+            moveState = MonsterMoveState.CHASING;
+        }
+        else if (Vector3.Distance(transform.position, destination) < outgoingStopDist) {
+            navagent.isStopped = true;
+            moveState = MonsterMoveState.IDLE;
+        }
+    }
+
+    private void Chase() {
+        if (IsPlayerVisible(transform.position, playerTransform.position, outgoingRange)) {
+            SetDestination(playerTransform.position);
+        }
+        else if (Vector3.Distance(transform.position, destination) > outgoingRange) {
+            navagent.speed = defaultSpeed;
+            navagent.isStopped = true;
+            moveState = MonsterMoveState.IDLE;
+        }
+    }
+
+    private void Update() {
+        if (!movementEnabled)
+            return;
+
+        if (moveState == MonsterMoveState.SEEKING)
+            Seek();
+        else if (moveState == MonsterMoveState.CHASING)
+            Chase();
+        else if (moveState == MonsterMoveState.IDLE)
+            Idle();
+    }
 }
--- 3D Monster Controller/Assets/Scripts/SkeletonController.cs
+++ 3D Monster Controller/Assets/Scripts/SkeletonController.cs
@@ -2,9 +2,14 @@
 
 public class SkeletonController : MonoBehaviour
 {
+    public float walkSpeed = 1;
+    public float runSpeed = 2;
+    public float idleTime = 60;
+
     private Animator anim;
     private Vector3 movement;
     private int attackStateHash;
+    private int skillStateHash;
     private float horizontalMovement;
     private float verticalMovement;
 
@@ -14,42 +19,79 @@ public class SkeletonController : MonoBehaviour
         movement = Vector3.zero;
 
         attackStateHash = Animator.StringToHash("Attack");
+        skillStateHash = Animator.StringToHash("Skill");
         horizontalMovement = 0;
         verticalMovement = 0;
     }
 	
     private void Attack() 
     {
-        // play attack animation
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
             anim.Play(attackStateHash);
+    }
+
+    private void UseSkill() 
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            anim.Play(skillStateHash);
+    }
+
+    private void Run() 
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+            anim.SetBool("Running", true);
+        else
+            anim.SetBool("Running", false);
+    }
+
+    private void Idle()
+    {
+        idleTime += Time.deltaTime;
+
+        if (idleTime >= 5)
+        {
+            idleTime = 0;
+
+            anim.SetInteger("IdleCounter", anim.GetInteger("IdleCounter") + 1);
+
+            if (anim.GetInteger("IdleCounter") > 5)
+                anim.SetInteger("IdleCounter", 0);
+        }
     }
 
     private void Move() 
     {
-        // prevent movement if attacking
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
-            return;
-
         horizontalMovement = Input.GetAxis("Horizontal");
         verticalMovement = Input.GetAxis("Vertical");
-        
+
+        anim.SetFloat("HorizontalSpeed", horizontalMovement);
+        anim.SetFloat("VerticalSpeed", verticalMovement);
+
+        // stop movement
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack") ||
+            anim.GetCurrentAnimatorStateInfo(0).IsName("Skill")) 
+        {
+            return;
+        }
+
         if (Input.GetButton("Horizontal") && !Input.GetButton("Vertical") ||
             Input.GetButton("Vertical") && !Input.GetButton("Horizontal")) 
         {
             // move horizontally / vertically
+            float speed = anim.GetBool("Running") ? runSpeed : walkSpeed;
             movement = new Vector3(horizontalMovement, 0, verticalMovement);
-            transform.Translate(movement * Time.deltaTime, Space.World);
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
         }
         else if (Input.GetButton("Horizontal") && Input.GetButton("Vertical")) 
         {
             // move diagonally
+            float speed = anim.GetBool("Running") ? runSpeed : walkSpeed;
             float diagonalXSpeed = horizontalMovement * 0.725f;
             float diagonalZSpeed = verticalMovement * 0.725f;
 
             movement = new Vector3(diagonalXSpeed, 0, diagonalZSpeed);
-            transform.Translate(movement * Time.deltaTime, Space.World);
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
         }
         else
@@ -57,15 +99,14 @@ public class SkeletonController : MonoBehaviour
             // retain facing direction when not moving
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
         }
-        
-        // update state machine vars
-        anim.SetFloat("HorizontalSpeed", horizontalMovement);
-        anim.SetFloat("VerticalSpeed", verticalMovement);
     }
 
 	private void Update ()
     {
         Attack();
+        UseSkill();
         Move();
+        Run();
+        Idle();
 	}
 }
